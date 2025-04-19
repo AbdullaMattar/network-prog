f6aa5046c9654a39521aa23636f4bc1b136dd182
AbdullaMattar <46789693+AbdullaMattar@users.noreply.github.com>
2025-04-19 22:42:13 +0300

    Update hw1_client.c

--- hw1_client.c
+++ hw1_client.c
@@ -1,111 +1,104 @@
+//HW1
+// Names: Abdulla Matar (151605) + Obada Gharaibeh (152818)
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
 #include <unistd.h>
 #include <signal.h>
 #include <arpa/inet.h>
-#include <sys/socket.h>
 
-#define MAXSTR 80
-#define BUFSIZE 256
+#define MAX_LEN 80
 
 int sockfd;
 
-void sigquit_handler(int sig) {
-    printf("\nClient_side > Good bye\n");
+// Handle Ctrl+\ quit signal
+void handle_quit(int sig) {
+    printf("\nGoodbye!\n");
+    usleep(100000);
+    (void)sig;
     close(sockfd);
     exit(0);
 }
 
 int main(int argc, char *argv[]) {
-    if(argc != 3) {
-        fprintf(stderr, "Usage: %s <server-ip> <server-port>\n", argv[0]);
-        exit(EXIT_FAILURE);
+    struct sockaddr_in server_addr;
+    char input[MAX_LEN + 1];
+    char choice[10];
+    char buffer[256];
+    int option;
+    unsigned char arg = 0;
+
+    // Check for correct number of arguments
+    if (argc != 3) {
+        printf("Usage: %s <server_ip> <server_port>\n", argv[0]);
+        return 1;
     }
-    
-    struct sockaddr_in serv_addr;
-    int port = atoi(argv[2]);
-    
-    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("Socket error");
-        exit(EXIT_FAILURE);
-    }
-    
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(port);
-    if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) <= 0) {
-        perror("Invalid address");
-        exit(EXIT_FAILURE);
-    }
-    memset(serv_addr.sin_zero, 0, sizeof(serv_addr.sin_zero));
-    
-    if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        perror("Connect error");
-        exit(EXIT_FAILURE);
-    }
-    
-    signal(SIGQUIT, sigquit_handler);
-    
-    char inputStr[MAXSTR+1];
-    char menuChoice[10];
-    while(1) {
-        printf("Client_side> please enter a string: ");
-        if(fgets(inputStr, sizeof(inputStr), stdin) == NULL)
-            break;
-        inputStr[strcspn(inputStr, "\n")] = 0;
-        
-        int operation;
-        unsigned char arg = 0;
-        while(1) {
-            printf("Client_side> select an operation:\n");
-            printf("  1. Change string to capital letters\n");
-            printf("  2. Count number of characters\n");
-            printf("  3. Count frequency of a character\n");
-            printf("  4. Enter another string\n");
-            printf("Your choice: ");
-            if(fgets(menuChoice, sizeof(menuChoice), stdin) == NULL)
-                continue;
-            operation = atoi(menuChoice);
-            if(operation >= 1 && operation <= 4)
-                break;
-            printf("Invalid choice. Try again.\n");
+
+    signal(SIGQUIT, handle_quit); // Register signal handler
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); // Create TCP socket
+
+    // Setup server address
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(atoi(argv[2]));
+    inet_pton(AF_INET, argv[1], &server_addr.sin_addr);
+    memset(&(server_addr.sin_zero), 0, 8);
+
+    // Connect to server
+    connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+
+    while (1) {
+        printf("Enter a string: ");
+        fgets(input, MAX_LEN, stdin);
+        input[strcspn(input, "\n")] = '\0'; // Remove newline
+
+        // Show operation menu
+        printf("Choose an operation:\n");
+        printf("1. Convert to UPPERCASE\n");
+        printf("2. Count characters\n");
+        printf("3. Count frequency of a character\n");
+        printf("4. Enter new string\n");
+
+        printf("Your choice: ");
+        fgets(choice, sizeof(choice), stdin);
+        option = atoi(choice);
+
+        if (option == 4) continue;
+
+        if (option == 3) {
+            // Get character to count
+            printf("Enter character to count: ");
+            arg = getchar();
+            while (getchar() != '\n'); // Clear input buffer
         }
-        
-        if(operation == 4)
-            continue;
-        
-        if(operation == 3) {
-            printf("Client_side> Enter the character: ");
-            char ch = getchar();
-            while(getchar() != '\n');
-            arg = ch;
-        }
-        
-        char request[BUFSIZE];
-        request[0] = (unsigned char) operation;
-        request[1] = arg;
-        strcpy(request+2, inputStr);
-        
-        printf("Client_side> sending \"%s\" to the server with requested operation %d\n", inputStr, operation);
-        write(sockfd, request, strlen(request+2) + 2);
-        
-        char response[BUFSIZE];
-        ssize_t n = read(sockfd, response, sizeof(response)-1);
-        if(n <= 0) {
+
+        // Build request buffer
+        buffer[0] = (unsigned char)option;
+        buffer[1] = arg;
+        strcpy(buffer + 2, input);
+        printf("sending \"%s\" to the server with requested operation %d \n", input , option);
+        write(sockfd, buffer, strlen(input) + 2); // Send to server
+
+        char response[256];
+        int n = read(sockfd, response, sizeof(response)); // Read response
+        if (n <= 0) {
             perror("Read error");
             break;
         }
+
         response[n] = '\0';
         unsigned char status = response[0];
         char *result = response + 1;
-        if(status == 0)
-            printf("Client_side> received \"%s\" from the server\n", result);
-        else
-            printf("Client_side> error: \"%s\"\n", result);
+
+        // Check status and print result
+        if (status == 0) {
+            printf("received: \"%s\" from the server\n", result);
+        } else {
+            printf("Error: %s\n", result);
+        }
     }
-    
-    close(sockfd);
+
+    close(sockfd); // Cleanup
     return 0;
 }
-
